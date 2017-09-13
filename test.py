f9fec3a3e228ef1df11a3425212296853e45ce96
Roman Miroshnychenko <romanvm@yandex.ru>
2017-09-13 22:20:39 +0300

    Test headless Chrome #7

--- test.py
+++ test.py
@@ -5,14 +5,17 @@
 # E-mail: romanvm@yandex.ua
 
 from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def test_headless_chrome():
     print('Starting Headless Chrome test...')
+    capabilities = DesiredCapabilities.CHROME
+    capabilities['loggingPrefs'] = {'browser': 'ALL'}
     options = ChromeOptions()
     options.add_argument('headless')
     options.add_argument('disable-gpu')
-    browser = Chrome(chrome_options=options)
+    browser = Chrome(chrome_options=options, desired_capabilities=capabilities)
     browser.get('http://example.com/')
     h1 = browser.find_element_by_tag_name('h1')
     print(h1.text)
